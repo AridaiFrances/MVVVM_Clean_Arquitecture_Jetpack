9eb94273bd73bfe06f47c20b1f84e4ccfff01883
Aridai Francés <afrances@intermarkit.es>
2022-05-08 09:51:04 +0200

    Initial commit

---
+++ app/src/main/java/com/ari/mvvvm_clean_arquitecture_jetpack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ari.mvvvm_clean_arquitecture_jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
